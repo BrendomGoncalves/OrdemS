c787f6a96c48eaa2b49868cb652d17de022af76e
BrendomGoncalves <brendom.vi@gmail.com>
2024-11-05 23:17:30 -0300

    feat: Adicionando component lista de categorias e serviço

---
+++ src/app/pages/categorias-lista/categorias-lista.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categorias-lista',
+  standalone: true,
+  imports: [],
+  templateUrl: './categorias-lista.component.html',
+  styleUrl: './categorias-lista.component.css'
+})
+export class CategoriasListaComponent {
+
+}
