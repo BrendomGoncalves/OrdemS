76c0e720c4186ce164f77ede71d96f66275936b0
BrendomGoncalves <brendom.vi@gmail.com>
2024-11-05 23:16:36 -0300

    feat: Adicionando rota Categorias

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import {PaginaInicialComponent} from './pages/pagina-inicial/pagina-inicial.comp
 import {ClientesListaComponent} from './pages/clientes-lista/clientes-lista.component';
 import {OrdemServicoComponent} from './pages/ordem-servico/ordem-servico.component';
 import {ServicosListaComponent} from './pages/servicos-lista/servicos-lista.component';
+import {CategoriasListaComponent} from './pages/categorias-lista/categorias-lista.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'pagina-inicial', pathMatch: 'full' },
   { path: 'pagina-inicial', component: PaginaInicialComponent },
   { path: 'clientes', component: ClientesListaComponent },
   { path: 'servicos', component: ServicosListaComponent},
+  { path: 'categorias', component: CategoriasListaComponent},
   { path: 'os', component: OrdemServicoComponent},
   { path: '**', redirectTo: 'pagina-inicial' }
 ];
