8b50bca5afe5ab21aa1c8f8d941dbbf835c22456
BrendomGoncalves <brendom.vi@gmail.com>
2024-11-08 15:41:45 -0300

    feat: Criando serviço para categorias

--- src/app/services/categoria/categorias.service.ts
+++ src/app/services/categoria/categorias.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
+import {environment} from '../../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {map, Observable} from 'rxjs';
+import {Categoria} from '../../models/categoria';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriasService {
+  private apiUrl = `${environment.apiUrl}/categorias`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getCategorias(): Observable<Categoria[]> {
+    return this.http.get<Categoria[]>(this.apiUrl).pipe(
+      map(categorias => categorias.sort((a, b) => a.nome.localeCompare(b.nome)))
+    );
+  }
+
+  getCategoriaById(id: string): Observable<Categoria> {
+    return this.http.get<Categoria>(`${this.apiUrl}/${id}`);
+  }
+
+  addCategoria(categoria: Categoria): Observable<Categoria> {
+    return this.http.post<Categoria>(this.apiUrl, categoria).pipe(
+      map(categoriaAdicionado => {
+        if(categoriaAdicionado.id !== undefined) {
+          categoriaAdicionado.id = categoriaAdicionado.id.toString();
+        }
+        return categoriaAdicionado;
+      })
+    )
+  }
+
+  updateCategoria(id: number | undefined, categoria: Categoria): Observable<Categoria>{
+    return this.http.put<Categoria>(`${this.apiUrl}/${id}`, categoria).pipe(
+      map(categoriaAtualizado => {
+        return categoriaAtualizado;
+      })
+    );
+  }
+
+  deleteCategoria(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+
+  getQuantidadeCategorias(): Observable<number> {
+    return this.http.get<Categoria[]>(this.apiUrl).pipe(
+      map(categorias => categorias.length)
+    );
+  }
 }
