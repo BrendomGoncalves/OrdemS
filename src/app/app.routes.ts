8bb5a6bc0acbdd61861b146e38826b88725c6344
BrendomGoncalves <brendom.vi@gmail.com>
2024-11-22 15:16:59 -0300

    feat: adicionando nova rota de venda de produtos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,14 @@ import {OrdemServicoComponent} from './pages/ordem-servico/ordem-servico.compone
 import {ServicosListaComponent} from './pages/servicos-lista/servicos-lista.component';
 import {CategoriasListaComponent} from './pages/categorias-lista/categorias-lista.component';
 import {ProdutosListaComponent} from './pages/produtos-lista/produtos-lista.component';
+import {VendaProdutoComponent} from './pages/venda-produto/venda-produto.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'pagina-inicial', pathMatch: 'full' },
   { path: 'pagina-inicial', component: PaginaInicialComponent },
   { path: 'clientes', component: ClientesListaComponent },
   { path: 'produtos', component: ProdutosListaComponent},
+  { path: 'venda-produto', component: VendaProdutoComponent },
   { path: 'servicos', component: ServicosListaComponent},
   { path: 'categorias', component: CategoriasListaComponent},
   { path: 'os', component: OrdemServicoComponent},
