3dc1239ae77a69e700d083c6ae8093ae199b99dd
BrendomGoncalves <brendom.vi@gmail.com>
2024-11-25 21:24:49 -0300

    feat: Adicionando nova função ao utils

--- src/app/ferramentas/utils.ts
+++ src/app/ferramentas/utils.ts
@@ -1,3 +1,6 @@
+import {AbstractControl, AsyncValidatorFn, ValidationErrors} from '@angular/forms';
+import {delay, map, Observable, of} from 'rxjs';
+
 /**
  * Gera um id unico.
  * @example const id = generateUniqueId();
@@ -5,4 +8,20 @@
  */
 export function generateUniqueId(): string {
   return Math.random().toString(36).slice(2, 9);
+}
+
+/**
+ * Função assíncrona que valida um campo.
+ * @example nome: ['', [Validators.required], [asyncValidator()]]
+ * @returns Um observable com o resultado da validação.
+ */
+export function asyncValidator(): AsyncValidatorFn {
+  return (control: AbstractControl): Observable<ValidationErrors | null> => {
+    return of(control.value).pipe(
+      delay(1000),
+      map(value => {
+        return value === 'invalid' ? {invalidAsync: true} : null;
+      })
+    );
+  };
 }
