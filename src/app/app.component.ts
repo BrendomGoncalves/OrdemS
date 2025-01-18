44796c112d50fa4418692a6bf7327261411a77d9
BrendomGoncalves <brendom.vi@gmail.com>
2025-01-18 13:29:18 -0300

    feat: Tradução data primeNg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {NavBarComponent} from './components/nav-bar/nav-bar.component';
-import {MessageService} from 'primeng/api';
+import {MessageService, PrimeNGConfig} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +11,16 @@ import {MessageService} from 'primeng/api';
   styleUrl: './app.component.css',
   providers: [MessageService]
 })
-export class AppComponent {
-  title = 'OrdemS';
+export class AppComponent implements OnInit {
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+  ngOnInit() {
+    this.primengConfig.setTranslation({
+      dayNames: ["domingo", "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira", "sexta-feira", "sábado"],
+      dayNamesShort: ["dom", "seg", "ter", "qua", "qui", "sex", "sáb"],
+      dayNamesMin: ["D", "S", "T", "Q", "Q", "S", "S"],
+      monthNames: ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"],
+      monthNamesShort: ["jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"],
+    });
+  }
 }
