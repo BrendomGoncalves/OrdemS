3ba36f776393914e42ad3c166d9d996b34f42cae
BrendomGoncalves <brendom.vi@gmail.com>
2026-03-09 15:16:50 -0300

    refactor: Update categoria service to use CategoriaCreateDto and improve method signatures

--- src/app/services/categoria/categorias.service.ts
+++ src/app/services/categoria/categorias.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {environment} from '../../../environments/environment';
 import {HttpClient} from '@angular/common/http';
 import {map, Observable} from 'rxjs';
-import {Categoria} from '../../models/categoria';
+import {Categoria} from '../../models/categoria/categoria';
+import {CategoriaCreateDto} from '../../models/categoria/categoriaCreateDto';
 
 @Injectable({
   providedIn: 'root'
@@ -14,27 +15,26 @@ export class CategoriasService {
 
   async getCategorias(): Promise<Observable<Categoria[]>> {
     return this.http.get<Categoria[]>(this.apiUrl).pipe(
-      map(categorias => categorias.sort((a, b) => a.nome.localeCompare(b.nome)))
+      map(categorias => {
+        return categorias;
+      })
     );
   }
 
-  async getCategoriaById(id: string): Promise<Observable<Categoria>> {
+  async getCategoriaById(id: number): Promise<Observable<Categoria>> {
     return this.http.get<Categoria>(`${this.apiUrl}/${id}`);
   }
 
-  async addCategoria(categoria: Categoria): Promise<Observable<Categoria>> {
-    return this.http.post<Categoria>(this.apiUrl, categoria).pipe(
-      map(categoriaAdicionado => {
-        if (categoriaAdicionado.id !== undefined) {
-          categoriaAdicionado.id = categoriaAdicionado.id.toString();
-        }
-        return categoriaAdicionado;
+  async addCategoria(categoria: CategoriaCreateDto): Promise<Observable<CategoriaCreateDto>> {
+    return this.http.post<CategoriaCreateDto>(this.apiUrl, categoria).pipe(
+      map(categoriaCriada => {
+        return categoriaCriada;
       })
-    )
+    );
   }
 
-  async updateCategoria(id: number | undefined, categoria: Categoria): Promise<Observable<Categoria>> {
-    return this.http.put<Categoria>(`${this.apiUrl}/${id}`, categoria).pipe(
+  async updateCategoria(id: number, categoria: CategoriaCreateDto): Promise<Observable<CategoriaCreateDto>> {
+    return this.http.put<CategoriaCreateDto>(`${this.apiUrl}/${id}`, categoria).pipe(
       map(categoriaAtualizado => {
         return categoriaAtualizado;
       })
